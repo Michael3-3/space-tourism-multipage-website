31221a875c7bfcfa178c4d990c4c5af5b7f59be4
mikhel <mikhelpottella@gmailcom>
2024-08-31 11:55:50 +0530

    happy ending

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
     "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        homeDesktop:"url('../starter-code/assets/home/background-home-desktop.jpg')",
+        homeTablet:"url('../starter-code/assets/home/background-home-tablet.jpg')",
+        homeMobile:"url('../starter-code/assets/home/background-home-mobile.jpg')",
+
+        destinationDesktop:"url('../starter-code/assets/destination/background-destination-desktop.jpg')",
+        destinationTablet:"url('../starter-code/assets/destination/background-destination-tablet.jpg')",
+        destinationMobile:"url('../starter-code/assets/destination/background-destination-mobile.jpg')",
+        crewDesktop:"url('../starter-code/assets/technology/background-technology-desktop.jpg')",
+        crewTablet:"url('../starter-code/assets/technology/background-technology-tablet.jpg')",
+        crewMobile:"url('../starter-code/assets/technology/background-technology-mobile.jpg')",
+      },
+      fontFamily:{
+        "Barlow": ['"Barlow Condensed"', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
